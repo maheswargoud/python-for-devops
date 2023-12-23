3f07942796fa0749a7f806fa7f7c4fea3d3c3b2c
maheswargoud <maheswargoud@gmail.com>
2023-12-23 15:52:53 +0530

    five

---
+++ Day-05/README.md
@@ -0,0 +1,8 @@
+*** Command Line Variable ***
+command line pass the variables
+
+
+
+
+*** Environment Variable ***
+secrets pass in env variables
---
+++ Day-05/env.py
@@ -0,0 +1,14 @@
+import os
+# os.getenv("password")
+# os.getenv("apitoken")
+
+print(os.getenv("password"))
+print(os.getenv("apitoken"))
+
+
+"""
+export password="admin@123"
+export apitoken="hdfsfgjsflkadwjqdsfnsk"
+python env.py
+
+"""
