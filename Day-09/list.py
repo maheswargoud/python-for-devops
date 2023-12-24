a8801c46b952b000648d5bf029169b2fb951747e
maheswargoud <maheswargoud@gmail.com>
2023-12-24 10:38:52 +0530

    loop

---
+++ Day-09/list.py
@@ -0,0 +1,15 @@
+colors = ["yellow", "green", "white", "blue", "black"]
+
+for x in colors:
+    print(x)
+
+
+
+
+# colors = ["yellow", "green", "white", "blue", "black"]
+
+# for x in range(3):
+#     print(x)
+#     print(colors)
+
+
